918837f1b74182c1364916b19f61ec600d649594
SymonBezerra <simaobezerratranslate@gmail.com>
2024-06-27 13:06:29 -0300

    Adjacency Matrices impl

--- stella.hpp
+++ stella.hpp
@@ -119,10 +119,10 @@ namespace stella {
             virtual void addEdge(E* edge) = 0;
             virtual void addEdge(string label, string n1, string n2) = 0;
             virtual void addEdge(string label, string n1, string n2, int weight) = 0;
-            virtual E* getEdge(string label) = 0;
+            // virtual E* getEdge(string label) = 0;
             virtual N* getNode(string label) = 0;
             virtual std::vector<N*>& getAllNodes() = 0;
-            virtual std::map<string, E*>& getAllEdges() = 0;
+            // virtual std::map<string, E*>& getAllEdges() = 0;
     };
 
     template<typename N, typename E>
@@ -169,7 +169,7 @@ namespace stella {
                 E* edge = new E(label, node1, node2, weight);
                 edges.insert({edge->getLabel(), edge});
             }
-            E* getEdge(string label) override {
+            E* getEdge(string label) {
                 auto it = edges.find(label);
                 if (it != edges.end()) return it->second;
                 return nullptr;
@@ -182,7 +182,7 @@ namespace stella {
             std::vector<N*>& getAllNodes() override {
                 return nodes;
             }
-            std::map<string, E*>& getAllEdges() override {
+            std::map<string, E*>& getAllEdges() {
                 return edges;
             }
             ~AdjList() {
@@ -200,6 +200,179 @@ namespace stella {
         static_assert(std::is_base_of<DirectedEdge, E>::value, "E must be of type stella::DirectedEdge for directed graphs");
         public:
             DirectedAdjList(): AdjList<N, E>() {}
+    };
+
+    template<typename N, typename E>
+    class AdjMatrix {
+        static_assert(std::is_base_of<Edge, E>::value, "E must be of type stella::Edge for non-directed graphs");
+    protected:
+        void pushNode(int size) {
+            edges.push_back(vector<map<string, E*>>{});
+            for(int i = size - 1; i >= 0; i--) {
+                edges[i].push_back(map<string, E*>{});
+            }
+        }
+        vector<N*> nodes;
+        vector<vector<map<string, E*>>> edges;
+    public:
+        AdjMatrix() {}
+        void addNode(N* node) {
+            if (getNode(node->getLabel()))
+                throw std::invalid_argument("Node already exists: " + node->getLabel());
+            nodes.push_back(node);
+            pushNode(nodes.size());
+        }
+        void addNode(std::string label) {
+            if (getNode(label))
+                throw std::invalid_argument("Node already exists: " + label);
+            N* node = new N(label);
+            nodes.push_back(node);
+            pushNode(nodes.size());
+        }
+        void addEdge(E* edge) {
+        int n1 = getNodeIndex(edge->getN1()->getLabel());
+        int n2 = getNodeIndex(edge->getN2()->getLabel());
+        if (n1 < 0 || n2 < 0)
+            throw std::invalid_argument("Node labels not found: "
+            + edge->getN1()->getLabel() + " " + edge->getN2()->getLabel());
+            if (n1 > n2) edges[n1][n2].insert({edge->label, edge});
+            else edges[n2][n1].insert({edge->label, edge});
+        }
+
+        void addEdge(string label, string n1, string n2) {
+            int node1 = getNodeIndex(n1);
+            int node2 = getNodeIndex(n2);
+            if (node1 < 0 || node2 < 0)
+                throw std::invalid_argument("Node labels not found: " + n1 + " " + n2);
+            E* edge = new E(label, nodes[node1], nodes[node2], 1);
+            if (node1 > node2) edges[node1][node2].insert({edge->label, edge});
+            else edges[node2][node1].insert({edge->label, edge});
+        }
+
+        void addEdge(string label, string n1, string n2, int weight) {
+            int node1 = getNodeIndex(n1);
+            int node2 = getNodeIndex(n2);
+            if (node1 < 0 || node2 < 0)
+                throw std::invalid_argument("Node labels not found: " + n1 + " " + n2);
+            E* edge = new E(label, nodes[node1], nodes[node2], weight);
+            if (node2 > node1) edges[node1][node2].insert({edge->label, edge});
+            else edges[node2][node1].insert({edge->label, edge});
+        }
+            N* getNode(std::string label) {
+                for (N* node : nodes) {
+                    if (node->getLabel().compare(label) == 0) return node;
+                }
+                return nullptr;
+            }
+            int getNodeIndex(std::string label) {
+                auto it = nodes.begin();
+                int index = 0;
+                while (it != nodes.end()) {
+                    if ((*it)->getLabel() == label) return index;
+                    ++it;
+                    ++index;
+                }
+                return -1;
+            }
+            std::vector<N*>& getAllNodes() {
+                return nodes;
+            }
+            std::vector<std::vector<std::map<string, E*>>>& getAllEdges() {
+                return edges;
+            }
+            ~AdjMatrix() {
+                int size = nodes.size();
+                int dec = 0;
+                for (int i = 0; i < size; i++, dec++) {
+                    for (int j = 0; j < size - dec; j++) {
+                        map<string, E*>& edgeMap = edges[i][j];
+                        for (auto it = edgeMap.begin(); it != edgeMap.end(); ++it) {
+                            delete it->second;
+                        }
+                        edgeMap.clear();
+                    }
+                }
+
+                for (int i = 0; i < nodes.size(); i++)
+                    delete nodes[i];
+
+                edges.clear();
+                nodes.clear();
+            }
+    };
+
+    template<typename N, typename E>
+    class DirectedAdjMatrix : public AdjMatrix<N, E> {
+        static_assert(std::is_base_of<DirectedEdge, E>::value, "E must be of type stella::DirectedEdge for directed graphs");
+    protected:
+        void pushNode(int size) {
+            this->edges.push_back(vector<map<string, E*>>{});
+            for(int i = 0; i < size; i++) {
+                if (i != size - 1)
+                    this->edges[i].push_back(map<string, E*>{});
+                else
+                    for (int j = 0; j < size; j++)
+                        this->edges[i].push_back(map<string, E*>{});
+            }
+        }
+    public:
+        DirectedAdjMatrix() : AdjMatrix<N, E>() {}
+        void addNode(N* node) {
+            if (this->getNode(node->getLabel()))
+                throw std::invalid_argument("Node already exists: " + node->getLabel());
+            this->nodes.push_back(node);
+            pushNode(this->nodes.size());
+        }
+        void addNode(std::string label) {
+            if (this->getNode(label))
+                throw std::invalid_argument("Node already exists: " + label);
+            N* node = new N(label);
+            this->nodes.push_back(node);
+            pushNode(this->nodes.size());
+        }
+        void addEdge(E* edge) {
+            int n1 = this->getNodeIndex(edge->getN1()->getLabel());
+            int n2 = this->getNodeIndex(edge->getN2()->getLabel());
+            if (n1 < 0 || n2 < 0)
+                throw std::invalid_argument("Node labels not found: "
+                    + edge->getN1()->getLabel() + " " + edge->getN2()->getLabel());
+            this->edges[n1][n2].insert({edge->label, edge});
+        }
+        void addEdge(string label, string n1, string n2) {
+            int node1 = this->getNodeIndex(n1);
+            int node2 = this->getNodeIndex(n2);
+            if (node1 < 0 || node2 < 0)
+                throw std::invalid_argument("Node labels not found: " + n1 + " " + n2);
+            E* edge = new E(label, this->nodes[node1], this->nodes[node2], 1);
+            this->edges[n1][n2].insert({edge->label, edge});
+        }
+
+        void addEdge(string label, string n1, string n2, int weight) {
+            int node1 = this->getNodeIndex(n1);
+            int node2 = this->getNodeIndex(n2);
+            if (node1 < 0 || node2 < 0)
+                throw std::invalid_argument("Node labels not found: " + n1 + " " + n2);
+            E* edge = new E(label, this->nodes[node1], this->nodes[node2], weight);
+            this->edges[n1][n2].insert({edge->label, edge});
+        }
+        ~DirectedAdjMatrix() {
+            int size = this->nodes.size();
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    map<string, E*>& edgeMap = this->edges[i][j];
+                    for (auto it = edgeMap.begin(); it != edgeMap.end(); ++it) {
+                        delete it->second;
+                    }
+                    edgeMap.clear();
+                }
+            }
+
+            for (int i = 0; i < this->nodes.size(); i++)
+                delete this->nodes[i];
+
+            this->edges.clear();
+            this->nodes.clear();
+        }
     };
 };
 #endif
