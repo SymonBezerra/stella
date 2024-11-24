fa7379b512b3153c185b160c0fc040e793295efe
SymonBezerra <simaobezerratranslate@gmail.com>
2024-11-24 11:32:14 -0300

    minor optimization

--- stella/adjlist.py
+++ stella/adjlist.py
@@ -9,10 +9,9 @@ class AdjList(_stella.AdjList):
     def _dfs_rec(self, start, dfs_tree):
         adj = [edge for edge in self.edges.values() if (edge.n1.label == start or edge.n2.label == start) and not edge.label in dfs_tree.edges]
         for i in adj:
-            a = self.edges[i.label]
-            next_node = a.n1 if a.n2.label == start else a.n2
+            next_node = i.n1 if i.n2.label == start else i.n2
             if not next_node in dfs_tree.nodes:
                 dfs_tree.add_node(next_node)
-                dfs_tree.add_edge(a)
+                dfs_tree.add_edge(i)
                 self._dfs_rec(next_node.label, dfs_tree)
         return dfs_tree
